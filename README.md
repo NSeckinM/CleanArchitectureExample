8822b179bd9484b117a9840ed48ec02b20ca1399
NseckinM <seckinmantar@gmail.com>
2021-09-28 18:36:26 +0300

    Interfaces => IAsyncRepository

---
+++ README.md
@@ -0,0 +1,28 @@
+# Clean Architecture Example
+A sample ASP.NET Project with Clean Architecture based on https://github.com/dotnet-architecture/eShopOnWeb.
+
+## Packages Installed
+### Web
+
+### Infrastructure
+Install-Package Microsoft.EntityFrameworkCore
+
+Install-Package Npgsql.EntityFrameworkCore.PostgreSQL
+
+Install-Package Ardalis.Specification.EntityFrameworkCore
+
+Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore
+
+### ApplicationCore
+Install-Package Ardalis.Specification
+
+## EF Commands
+**Default project: src\Infrastructure**
+
+Add-Migration InitialIdentity -Context AppIdentityDbContext -OutputDir "Identity/Migrations"
+
+Update-Database -Context AppIdentityDbContext
+
+Add-Migration InitialApp -Context ApplicationDbContext -OutputDir "Data/Migrations"
+
+Update-Database -Context ApplicationDbContext 
---
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,49 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{                            
+                                         /*Constraint(kısıtlama) içine girebilecek veri türü base class dan miras almış olmalı*/
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        //(metotlar uygulanırken hangi teknolojiyi kullandığı bizi bağlamıyor infrada belirlenin rahatlıkla değiştirebilmek için interface yapısını kullanıyoruz.)
+
+        //Veriyi Id ye göre Getirme
+        Task<T> GetById(int Id);
+
+        // T türü seçilen verinin bütün listesini döndürür.
+        Task<List<T>> ListAllAsync();
+
+        // T türü seçilen veriyi belirttiğimiz filitreye göre listeler.
+        Task<List<T>> ListAsync(ISpecification<T> specification);
+
+        // T türü seçilen ürünü DB'e ekler ve ürünün kendisini geri döndürür çünkü eklediğimiz ürünün Id sini bilmiyoruz otomatik üretiliyor geri döndürülmesi bize fayda sağlıyor.
+        Task<T> AddAsync(T entity);
+
+        //T türünde verilen parametreyi günceller.
+        Task UpdateAsync(T entity);
+
+        //T türünde verilen parametreyi DB den siler.
+        Task DeleteAsync(T entity);
+
+        // verilerin sayısını alabiliyoruz ama burada da specification ekleyebiliyoruz
+        // EXP: Adı "A" ile başlayan ürünlerin sayısı diyebileceğiz.
+        Task CountAsync(ISpecification<T> specification);
+
+        //Specification sartına uyan ilk veriyi getirecek.
+        Task FirstAsync(ISpecification<T> specification);
+
+        //Specification sartına uyan ilk veriyi getirecek bulamazsa hata vermeyip default değeri gönderecek.
+        Task FirsOrDefaulttAsync(ISpecification<T> specification);
+
+
+
+
+
+    }
+}
